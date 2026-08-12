2e6713c9887cd420f66fb69a552d5e67f7b6ffcd
Rosa Donoso <rosa.donoso@westernunion.com>
2026-08-12 12:21:54 +0200

    add new modal

--- GhibliFinder/src/components/MovieModal.jsx
+++ GhibliFinder/src/components/MovieModal.jsx
@@ -1,20 +1,24 @@
 const MovieModal = ({ movie, onClose }) => {
-  const trailerId = 'oTUush1wXL0';
-  const URLYoutube = `https://www.youtube.com/embed/${trailerId};`;
+  // const trailerId = 'oTUush1wXL0';
+  // const URLYoutube = `https://www.youtube.com/embed/${trailerId};`;
+
   return (
     <div className="modal-overlay">
-      {/* <div className="modal-content"> */}
-      <iframe
-        className="modal-video"
-        src={URLYoutube}
-        title={`Tráiler de ${movie.title}`}
-        allow="fullscreen"
-        onClick={onClose}
-      />
+      <div className="modal-video">
+        <div className="modal-image-container">
+          <img className="modal-image" src={movie.image} alt={movie.title} />
+
+          <div className="modal-image-overlay">
+            <div className="modal-titles">
+              <h1>{movie.original_title}</h1>
+              <h2>{movie.original_title_romanised}</h2>
+            </div>
+          </div>
+        </div>
+      </div>
       <button className="modal-close" onClick={onClose}>
         X
       </button>
-      {/* </div> */}
     </div>
   );
 };
