a2662bc47393e1945720f1e14a674105f3a7521f
Rosa Donoso <rosa.donoso@westernunion.com>
2026-08-12 11:28:19 +0200

    changes staged

--- GhibliFinder/src/App.jsx
+++ GhibliFinder/src/App.jsx
@@ -1,4 +1,4 @@
-git import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import MovieCard from './components/MovieCard';
 import { FaHeart } from 'react-icons/fa';
 import MovieModal from './components/MovieModal';
