d943bd548a1a5fa8c289f9c1d2d2141eec84cd2b
Rosa Donoso <rosa.donoso@westernunion.com>
2026-08-13 13:59:39 +0200

    add literal objet to get the season

--- recetarium/src/components/Navbar.jsx
+++ recetarium/src/components/Navbar.jsx
@@ -1,22 +1,51 @@
 import { Link } from 'react-router-dom';
-import { GiHotMeal } from 'react-icons/gi';
+import { getCurrentSeason } from '@postlight/seasons';
 
 export function Navbar() {
+  const seasonalRoutes = {
+    summer: [
+      { path: '/recetas-verano', title: 'Recetas de verano' },
+      { path: '/recetas-otono', title: 'Recetas de otono' },
+      { path: '/recetas-invierno', title: 'Recetas de invierno' },
+      { path: '/recetas-primavera', title: 'Recetas de primavera' },
+    ],
+    winter: [
+      { path: '/recetas-invierno', title: 'Recetas de invierno' },
+      { path: '/recetas-primavera', title: 'Recetas de primavera' },
+      { path: '/recetas-verano', title: 'Recetas de verano' },
+      { path: '/recetas-otono', title: 'Recetas de otono' },
+    ],
+    spring: [
+      { path: '/recetas-primavera', title: 'Recetas de primavera' },
+      { path: '/recetas-verano', title: 'Recetas de verano' },
+      { path: '/recetas-otono', title: 'Recetas de otono' },
+      { path: '/recetas-invierno', title: 'Recetas de invierno' },
+    ],
+    autumn: [
+      { path: '/recetas-otono', title: 'Recetas de otono' },
+      { path: '/recetas-invierno', title: 'Recetas de invierno' },
+      { path: '/recetas-primavera', title: 'Recetas de primavera' },
+      { path: '/recetas-verano', title: 'Recetas de verano' },
+    ],
+    default: [
+      { path: '/recetas-primavera', title: 'Recetas de primavera' },
+      { path: '/recetas-verano', title: 'Recetas de verano' },
+      { path: '/recetas-otono', title: 'Recetas de otono' },
+      { path: '/recetas-invierno', title: 'Recetas de invierno' },
+    ],
+  };
+  const currentSeason = getCurrentSeason(new Date());
+
+  const currentRoutesAndTitles = seasonalRoutes[currentSeason];
+
   return (
     <nav className="bg-pink-500 text-white ">
       <ul className="flex flex-row justify-around">
-        <li>
-          <Link to="/recetas-verano">Recetas de verano</Link>
-        </li>
-        <li>
-          <Link to="/recetas-invierno">Recetas de invierno</Link>
-        </li>
-        <li>
-          <Link to="/recetas-primavera">Recetas primaverales</Link>
-        </li>
-        <li>
-          <Link to="/recetas-otono">Recetas de otonito</Link>
-        </li>
+        {currentRoutesAndTitles?.map((route) => (
+          <li key={route.path}>
+            <Link to={route.path}>{route.title}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
